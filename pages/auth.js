121f84513c35cd08ed03be3fb34ec1017ae73f44
arbhassan <hassanjawad1321@gmail.com>
2021-09-21 12:33:23 +0500

    Added Auth page and logic for auth

--- pages/auth.js
+++ pages/auth.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 import Head from "next/head";
+
+import { LockClosedIcon } from "@heroicons/react/solid";
 
 import client from "../client";
 import groq from "groq";
@@ -18,6 +23,7 @@ export async function getServerSideProps() {
 
     "footer": *[_type == 'footer'],
     "contact": *[_type == 'contact'],
+    "password": *[_type == 'password'],
 
   }
   `;
@@ -31,16 +37,81 @@ export async function getServerSideProps() {
       footer: data.footer,
       navbar: data.navbar,
       contact: data.contact,
+      password1: data.password,
     },
   };
 }
 
-function Auth() {
+function Auth({ password1 }) {
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  function handleSubmit(event) {
+    if (password == password1[0].password) {
+      router.push("/secret");
+
+      event.preventDefault();
+    } else {
+      event.preventDefault();
+      return <p>Wrong password</p>;
+    }
+  }
+
   return (
     <div>
       <Head>
-        <title>Contact Info | RM of Willowdale No. 153</title>
+        <title>Auth Page | RM of Willowdale No. 153</title>
       </Head>
+      <div className="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography", require("@tailwindcss/forms"))],
 };
